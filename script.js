94fe52e9f6c6aee6d57a16179493a8c1ea68effc
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2021-09-24 08:45:24 +0000

    Created Ball

--- script.js
+++ script.js
@@ -8,10 +8,24 @@ class Ball{
    this.vx = vx;
    this.vy = vy;
    this.color = _color;
+  } 
+
+  drawball() {
+   fill(this.color);
+   ellipse(this.x, this.y, this.w, this.h); 
   }
-}
+} 
+
+var ball1;
 
 function setup() {
   createCanvas(450, 650);
+  
+  ball1 = new Ball(225, 625, 50, 50, 0, 0, "white");
+}
+
+function draw() {
   background("grey");
+
+  ball1.drawball();
 }
