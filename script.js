6e251942d68ea3fce93b280704b874d84b025721
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2021-09-29 10:38:36 +0000

    bal stuiter met gravity

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 class Ball{
 
-  constructor(x, y, w, h, vx, vy, _color){
+  constructor(x, y, w, h, v, gravity, _color){
    this.x = x;
    this.y = y;
    this.w = w;
    this.h = h;
-   this.vx = vx;
-   this.vy = vy;
+   this.v = v;
+   this.g = gravity;
    this.color = _color;
   } 
 
   drawball() {
    fill(this.color);
-   ellipse(this.x, this.y, this.w, this.h); 
-   this.y = this.vy + this.y;
+   ellipse(this.x, this.y, this.w, this.h);
+     this.y = this.v + this.y;
+     this.v = this.v + this.g;
 
    if(this.y > 650){
-     this.y = 0;
+     this.v = -0.95 * this.v;
    }
 
    if(this.x > 450) {
@@ -27,18 +28,44 @@ class Ball{
   }
 } 
 
-var ball1;
+class PLatform{
+
+  constructor(x, y, h, w, vx, vy, _color){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.color = _color;
+  }
+
+  drawplatform() {
+    fill(this.color);
+    rect(this.x, this.y, this.w, this.h)
+    this.y = this.vy + this.y;
+  }
+
+}
+
+
+var ball1, platform1;
 
 function setup() {
   createCanvas(450, 650);
   
-  ball1 = new Ball(225, 0, 50, 50, 0, 5, "white");
+  ball1 = new Ball(225, 0, 50, 50, 1, 0.2, "white");
+
+  platform1 = new PLatform(0, 300, 10, 200, 0, 0, "black");
+  
 }
 
 function draw() {
   background("grey");
 
   ball1.drawball();
+
+  platform1.drawplatform();
 
   if (keyIsPressed) {
    if (keyCode == RIGHT_ARROW) {
